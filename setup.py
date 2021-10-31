9e2b8f3480426aab706ffe798ec8e5442c44d8cd
Monal Kumar <Monalkumar745@gmail.com>
2021-10-31 13:59:23 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src", version="0.0.1",
+    description="This is a CNN classification with DVC tracking",
+    author="Monalsingh",
+    packages=find_packages(),
+    license="MIT"
+
+)
